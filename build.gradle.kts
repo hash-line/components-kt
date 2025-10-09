95131c0161324d7779cde07161e896a6e7b7f96b
Hashim Ali Munir <hashimalimunir@gmail.com>
2025-10-09 14:40:33 +0500

    initialize project with Gradle and Kotlin setup
    # Conflicts:
    #	.gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "co.hashline"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(24)
+}

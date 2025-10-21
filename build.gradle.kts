b12993a77330b9bbc251cf6f5e32bc7110bb7ff3
Hashim Ali Munir <hashimalimunir@gmail.com>
2025-10-21 17:26:37 +0500

    refactor: update build configuration for Maven Central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,16 @@
 plugins {
     kotlin("jvm") version "2.0.21"
     alias(libs.plugins.kotlin.serialization)
-    `maven-publish`
+    `java-library`
+    signing
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
 group = "co.hashline"
-version = "1.0"
+version = "0.9.0"
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://maven.pkg.github.com/hash-line/events-kt")
-        credentials {
-            username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-            password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-        }
-    }
 }
 
 dependencies {
@@ -30,7 +25,6 @@ dependencies {
     // Add kotlinx-datetime dependency
     implementation(libs.jetbrains.kotlinx.datetime)
 
-
     testImplementation(kotlin("test"))
 }
 
@@ -41,65 +35,39 @@ kotlin {
     jvmToolchain(21)
 }
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
+// Maven Central publishing configuration
+mavenPublishing {
+    // Always sign all publications
+    signAllPublications()
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "21"
-    }
-}
+    coordinates("co.hashline", "components-kt", version.toString())
 
-// Publishing configuration
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-            
-            groupId = "co.hashline"
-            artifactId = "components-kt"
-            version = "1.0"
-            
-            pom {
-                name.set("Components-KT")
-                description.set("A comprehensive Kotlin library for building reusable UI components")
-                url.set("https://github.com/hash-line/components-kt")
-                
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-                
-                developers {
-                    developer {
-                        id.set("hashline")
-                        name.set("Hashline")
-                        email.set("contact@hashline.co")
-                    }
-                }
-                
-                scm {
-                    connection.set("scm:git:git://github.com/hash-line/components-kt.git")
-                    developerConnection.set("scm:git:ssh://github.com:hash-line/components-kt.git")
-                    url.set("https://github.com/hash-line/components-kt")
-                }
+    // Enable Maven Central publishing
+    publishToMavenCentral()
+
+    pom {
+        name = "Components-KT"
+        description = "A lightweight Kotlin library for building Server driven UI components."
+        inceptionYear = "2025"
+        url = "https://github.com/hash-line/components-kt"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://opensource.org/licenses/MIT"
+                distribution = "repo"
             }
         }
-    }
-    
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/hash-line/components-kt")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+        developers {
+            developer {
+                id = "HashimAli09"
+                name = "Hashim Ali"
+                url = "https://github.com/HashimAli09"
             }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/hash-line/components-kt.git")
+            developerConnection.set("scm:git:ssh://github.com:hash-line/components-kt.git")
+            url.set("https://github.com/hash-line/components-kt")
         }
     }
 }
